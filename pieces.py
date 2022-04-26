c8be222669fdc5cdaf44070f106a1170215c3ba6
unknown <dario.patt2@gmail.com>
2022-04-26 18:26:32 +0200

    Initial Chess Setup

---
+++ pieces.py
@@ -0,0 +1,25 @@
+
+
+
+class Piece:
+    pass
+
+class King(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class Rook(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Kinght(Piece):
+    pass
+
+class Pawn(Piece):
+    pass
+
+
---
+++ server.py
@@ -0,0 +1,9 @@
+
+
+
+def client_thread():
+    pass
+
+
+if __name__ == "__main__":
+    pass
